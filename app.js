c117036c721fd06cee94dc78ebd64054a2a07e06
premkumarmookiah <136256349+premkumarmookiah@users.noreply.github.com>
2024-01-18 16:55:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,15 +2,37 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const rootDir = require('./utils/path');
-const firstAppRouter = require('./router/firstapp')
+const firstAppRouter = require('./router/firstapp');
 const app = express();
+
+// Use body-parser with extended option
 app.use(bodyParser.urlencoded({ extended: false }));
 
- app.use('/premkumar',firstAppRouter);
+app.use('/premkumar', firstAppRouter);
 
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+});
 
+// Add process event handlers for exit, SIGTERM, and SIGINT
+process.on('exit', (code) => {
+    console.log(`Process exiting with code ${code}`);
+});
 
- app.use((req,res,next)=>{
-    res.status(404).sendFile(path.join(rootDir,'views','404.html'))
- });
-app.listen(3000);
+process.on('SIGTERM', () => {
+    console.log('Received SIGTERM signal. Cleaning up...');
+    // Perform cleanup operations before exiting
+    process.exit(0);
+});
+
+process.on('SIGINT', () => {
+    console.log('Received SIGINT signal. Cleaning up...');
+    // Perform cleanup operations before exiting
+    process.exit(0);
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
