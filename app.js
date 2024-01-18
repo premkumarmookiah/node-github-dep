d9ade15fc54ebc0bd9ac608d7ced4d825fda0ad8
premkumarmookiah <136256349+premkumarmookiah@users.noreply.github.com>
2024-01-18 16:45:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const rootDir = require('./utils/path');
 const firstAppRouter = require('./router/firstapp')
 const app = express();
-
+app.use(bodyParser.urlencoded({ extended: false }));
 
  app.use('/premkumar',firstAppRouter);
 
@@ -13,5 +13,4 @@ const app = express();
  app.use((req,res,next)=>{
     res.status(404).sendFile(path.join(rootDir,'views','404.html'))
  });
- app.use(bodyParser.urlencoded());
 app.listen(3000);
